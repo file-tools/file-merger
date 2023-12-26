b6e5bbf3ffd8b95e5dcdc46d545233b3cbc833b3
R. Kennedy <rkennedy@surgostream.com>
2023-12-26 13:48:03 -0500

    Initial

---
+++ Config.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Ideas could add: 
+- FILE FORMATS: Currently only markdown files but could add a option to switch output file format and input ... 
+- CUSTOM OUTPUT FORMATS: If you want to customize the output formats to use Bullets or not include time or anything -- like adding a templating method ... currently limited to my preference of using DateTime at the top. 
+- Merge to one file instead of multiple (would need to add new header to distinquish days)
+- Change date ordering - ASC or DESC ... currently latest at top, my default
+*/
+
+// Set the timezone to New York
+date_default_timezone_set('America/New_York');
+
+// Define input and output paths
+$InputPath = '/Users/Me/Library/Mobile Documents/iCloud~co~fluder~fsnotes/Documents/_ToJournal';
+$OutputPath = '/Users/Me/Desktop/Out/';
+
+// Date formate for output files 
+$DateFormat = 'Y_m_d';
+
+
